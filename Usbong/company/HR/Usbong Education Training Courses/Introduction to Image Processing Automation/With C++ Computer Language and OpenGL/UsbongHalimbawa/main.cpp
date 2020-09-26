4029cbb9c046814bf89a7b4a981ca3a14e66b07b
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-09-26 08:39:33 +0800

    Add files via upload

---
+++ Usbong/company/HR/Usbong Education Training Courses/Introduction to Image Processing Automation/With C++ Computer Language and OpenGL/UsbongHalimbawa/main.cpp
@@ -0,0 +1,261 @@
+/*
+ * Copyright 2020 Usbong Social Systems, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author: Michael Syson
+ * @date created: 20200926
+ * @date updated: 20200926
+ *
+ * References:
+ * 1) Dev-C++ 5.11 auto-generated OpenGL example project
+ *
+ * 2) https://www.libsdl.org/download-2.0.php;
+ * last accessed: 20200423
+ *
+ * 3) https://docs.microsoft.com/en-us/windows/win32/inputdev/user-input3
+ * last accessed: 20200424
+ *
+ * 4) Astle, D. and Hawkin, K. (2004). "Beginning OpenGL Game Programming". USA: Premier Press.
+ *
+ * 5) https://en.wikibooks.org/wiki/OpenGL_Programming/Basics/Structure
+ * last accessed: 20200926
+ *
+ * 6) https://dos.gamebub.com/cpp.php
+ * last accessed: 20200921
+ *
+ * Notes:
+ * 1) We can use this software tool to extract the compressed (zipped) folder.
+ *   https://www.7-zip.org/download.html; last accessed: 20200423
+ *
+ * 2) OpenGL (Open Graphics Library) Utility Toolkit Library
+ *
+ */
+
+
+/**************************
+ * Includes
+ *
+ **************************/
+
+#include <windows.h>
+#include <gl/gl.h>
+
+
+/**************************
+ * Function Declarations
+ *
+ **************************/
+
+LRESULT CALLBACK WndProc (HWND hWnd, UINT message,
+WPARAM wParam, LPARAM lParam);
+void EnableOpenGL (HWND hWnd, HDC *hDC, HGLRC *hRC);
+void DisableOpenGL (HWND hWnd, HDC hDC, HGLRC hRC);
+
+
+/**************************
+ * WinMain
+ *
+ **************************/
+
+int WINAPI WinMain (HINSTANCE hInstance,
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpCmdLine,
+                    int iCmdShow)
+{
+    WNDCLASS wc;
+    HWND hWnd;
+    HDC hDC;
+    HGLRC hRC;        
+    MSG msg;
+    BOOL bQuit = FALSE;
+    float theta = 0.0f;
+
+    /* register window class */
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon (NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor (NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH) GetStockObject (BLACK_BRUSH);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = "GLSample";
+    RegisterClass (&wc);
+
+    /* create main window */
+	//edited by Mike, 20200926
+/*
+    hWnd = CreateWindow (
+      "GLSample", "OpenGL Sample", 
+      WS_CAPTION | WS_POPUPWINDOW | WS_VISIBLE,
+      0, 0, 256, 256,
+      NULL, NULL, hInstance, NULL);
+*/
+
+    hWnd = CreateWindow (
+      "GLSample", "Usbong OpenGL Halimbawa", 
+      WS_CAPTION | WS_POPUPWINDOW | WS_VISIBLE,
+      0, 0, 512, 512,
+      NULL, NULL, hInstance, NULL);
+
+    /* enable OpenGL for the window */
+    EnableOpenGL (hWnd, &hDC, &hRC);
+
+    /* program main loop */
+    while (!bQuit)
+    {
+        /* check for messages */
+        if (PeekMessage (&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            /* handle or dispatch messages */
+            if (msg.message == WM_QUIT)
+            {
+                bQuit = TRUE;
+            }
+            else
+            {
+                TranslateMessage (&msg);
+                DispatchMessage (&msg);
+            }
+        }
+        else
+        {
+            /* OpenGL animation code goes here */
+
+            glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
+            glClear (GL_COLOR_BUFFER_BIT);
+
+			//removed by Mike, 20200926
+/*
+            glPushMatrix ();			
+            glRotatef (theta, 0.0f, 0.0f, 1.0f);
+            glBegin (GL_TRIANGLES);
+            glColor3f (1.0f, 0.0f, 0.0f);   glVertex2f (0.0f, 1.0f);
+            glColor3f (0.0f, 1.0f, 0.0f);   glVertex2f (0.87f, -0.5f);
+            glColor3f (0.0f, 0.0f, 1.0f);   glVertex2f (-0.87f, -0.5f);
+            glEnd ();
+            glPopMatrix ();
+
+            SwapBuffers (hDC);
+
+            theta += 1.0f;
+            Sleep (1);
+*/            
+			/* Draws two horizontal lines */
+            glPushMatrix ();			
+			glBegin(GL_LINES);
+              glColor3f (0.0f, 1.0f, 0.0f);
+              //TO-DO: -add: grid
+              //note: one (1) line, two (2) vertices
+			  glVertex2f(0.5f, 0.5f); 
+			  glVertex2f(-0.5f, 0.5f); 
+			glEnd();            
+            glPopMatrix ();
+            SwapBuffers (hDC);
+            
+            //TO-DO: -add: read input file with vertices
+            //refer: https://all-things-andy-gavin.com/2020/02/27/war-stories-crash-bandicoot/
+            //last accessed: 20200926
+        }
+    }
+
+    /* shutdown OpenGL */
+    DisableOpenGL (hWnd, hDC, hRC);
+
+    /* destroy the window explicitly */
+    DestroyWindow (hWnd);
+
+    return msg.wParam;
+}
+
+
+/********************
+ * Window Procedure
+ *
+ ********************/
+
+LRESULT CALLBACK WndProc (HWND hWnd, UINT message,
+                          WPARAM wParam, LPARAM lParam)
+{
+
+    switch (message)
+    {
+    case WM_CREATE:
+        return 0;
+    case WM_CLOSE:
+        PostQuitMessage (0);
+        return 0;
+
+    case WM_DESTROY:
+        return 0;
+
+    case WM_KEYDOWN:
+        switch (wParam)
+        {
+        case VK_ESCAPE:
+            PostQuitMessage(0);
+            return 0;
+        }
+        return 0;
+
+    default:
+        return DefWindowProc (hWnd, message, wParam, lParam);
+    }
+}
+
+
+/*******************
+ * Enable OpenGL
+ *
+ *******************/
+
+void EnableOpenGL (HWND hWnd, HDC *hDC, HGLRC *hRC)
+{
+    PIXELFORMATDESCRIPTOR pfd;
+    int iFormat;
+
+    /* get the device context (DC) */
+    *hDC = GetDC (hWnd);
+
+    /* set the pixel format for the DC */
+    ZeroMemory (&pfd, sizeof (pfd));
+    pfd.nSize = sizeof (pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | 
+      PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 16;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+    iFormat = ChoosePixelFormat (*hDC, &pfd);
+    SetPixelFormat (*hDC, iFormat, &pfd);
+
+    /* create and enable the render context (RC) */
+    *hRC = wglCreateContext( *hDC );
+    wglMakeCurrent( *hDC, *hRC );
+
+}
+
+
+/******************
+ * Disable OpenGL
+ *
+ ******************/
+
+void DisableOpenGL (HWND hWnd, HDC hDC, HGLRC hRC)
+{
+    wglMakeCurrent (NULL, NULL);
+    wglDeleteContext (hRC);
+    ReleaseDC (hWnd, hDC);
+}
