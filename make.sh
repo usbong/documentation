b09800655db4386fe1d1cb275d43c6dba139376d
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-12-03 14:41:19 +0800

    Add files via upload

---
+++ make.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Usbong Halimbawa: Image Processing Automation
+# C++ Computer Language and OpenGL
+#
+# Compile and Link Multiple Source Files
+#
+# @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
+# @author: SYSON, MICHAEL B.
+# @date created: 20200930
+# @last updated: 20201202
+#
+# Reference:
+# 1) https://www3.ntu.edu.sg/home/ehchua/programming/cpp/gcc_make.html;
+# last accessed: 20200930
+
+g++ -c main.cpp
+g++ -c OpenGLCanvas.cpp
+g++ -c MyDynamicObject.cpp
+g++ -c RobotShip.cpp
+g++ -c Font.cpp
+g++ -c Beam.cpp
+g++ -c Asteroid.cpp
+g++ -c Level.cpp
+g++ -c PolygonUtils.cpp
+g++ -c CTargaImage.cpp
+
+#g++ -o mainOutput main.o OpenGLCanvas.o -lGL -lGLU -lglut
+g++ -o mainOutput main.o OpenGLCanvas.o MyDynamicObject.o RobotShip.o Font.o Beam.o Asteroid.o Level.o PolygonUtils.o CTargaImage.o -lGL -lGLU -lglut
