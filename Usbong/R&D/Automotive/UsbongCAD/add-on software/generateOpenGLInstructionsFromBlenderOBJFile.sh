4c8ee288a959e26f0785b1ecf1b48c9683b2ea6f
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-03-28 10:52:48 +0800

    Add files via upload

---
+++ Usbong/R&D/Automotive/UsbongCAD/add-on software/generateOpenGLInstructionsFromBlenderOBJFile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# @echo off
+# Copyright 2021 USBONG SOCIAL SYSTEMS, INC. (USBONG)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+#
+# @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
+# @author: SYSON, MICHAEL B.
+# @date created: 20210327
+# @last modified: 20210328
+# @website: http://www.usbong.ph
+
+#Windows Machine
+# java -cp .\software; generateOpenGLInstructionsFromBlenderOBJFile input/*.obj
+#Linux Machine
+java -cp ./software: generateOpenGLInstructionsFromBlenderOBJFile input/*.obj
+
+# PAUSE
