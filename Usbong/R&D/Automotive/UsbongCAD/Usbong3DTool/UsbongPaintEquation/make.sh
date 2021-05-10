2f34c0099328b648bdcdf193edf3e67d02def764
Masarap Mabuhay <masarapmabuhay@gmail.com>
2021-05-10 10:09:20 +0800

    Add files via upload

--- Usbong/R&D/Automotive/UsbongCAD/Usbong3DTool/UsbongPaintEquation/make.sh
+++ Usbong/R&D/Automotive/UsbongCAD/Usbong3DTool/UsbongPaintEquation/make.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Usbong Automotive: Image Processing Automation
+# Usbong Computer Aided Design (CAD): 3D Tool 
+# Usbong Paint Equation
+#
 # C++ Computer Language and OpenGL
 #
 # Compile and Link Multiple Source Files
@@ -8,7 +11,7 @@
 # @company: USBONG SOCIAL SYSTEMS, INC. (USBONG)
 # @author: SYSON, MICHAEL B.
 # @date created: 20200930
-# @last updated: 20210220
+# @last updated: 20210510
 #
 # Reference:
 # 1) https://www3.ntu.edu.sg/home/ehchua/programming/cpp/gcc_make.html;
@@ -35,7 +38,7 @@ g++ -c UsbongUtils.cpp
 #g++ -o mainOutput main.o OpenGLCanvas.o -lGL -lGLU -lglut
 # edited by Mike, 20210126
 # g++ -o ./output/mainOutputLinux mainLinux.o OpenGLCanvas.o MyDynamicObject.o RobotShip.o Font.o Beam.o Asteroid.o Level.o PolygonUtils.o CTargaImage.o Pilot.o UsbongUtils.o -lGL -lGLU -lglut
-g++ -o ./output/UsbongAutomotiveLinux mainLinux.o OpenGLCanvas.o MyDynamicObject.o RobotShip.o Font.o Beam.o Asteroid.o Level.o PolygonUtils.o CTargaImage.o Pilot.o UsbongUtils.o -lGL -lGLU -lglut
+g++ -o ./output/UsbongPaintEquationLinux mainLinux.o OpenGLCanvas.o MyDynamicObject.o RobotShip.o Font.o Beam.o Asteroid.o Level.o PolygonUtils.o CTargaImage.o Pilot.o UsbongUtils.o -lGL -lGLU -lglut
 
 #added by Mike, 20210220
 cp -r ./*.o ./objectFilesLinux/
